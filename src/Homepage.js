10b68c3e4d7d9c50efdbf07f75e62f6a7047326d
prakritip-csuf <prakritip@csu.fullerton.edu>
2025-10-04 10:05:09 -0700

    initial commit

---
+++ src/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <header>My Study Aid Website</header>
+      {/* Add more sections here */}
+    </div>
+  );
+}
+
+export default HomePage;
